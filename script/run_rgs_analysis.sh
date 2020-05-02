815ee41a742b4c69a9445ccf5835fa1f0f1a50bd
Lspeight <60630914+Lspeight@users.noreply.github.com>
2020-05-02 18:26:38 -0500

    Update run_rgs_analysis.sh

--- script/run_rgs_analysis.sh
+++ script/run_rgs_analysis.sh
@@ -8,12 +8,8 @@ fi
 # setup the fields for the temp_results.txt file. Using this temp file is a way to obtain the results
 # and then manipulate the data for a "pretty print", saving that "pretty print" to the final file
 # and then removing this temp file!
-echo "Lineage   Ploidy  Histone  pn  ps  pn/ps" > ../output/temp_PnPs_results.txt
-echo "-------   ------  -------  ---------------------- ---------------------- ---------------------" >> ../output/temp_PnPs_results.txt
-
-# and do the same for a dnds file, as well.
-echo "Lineage   Ploidy  Histone  dn  ds  dn/ds" > ../output/temp_DnDs_results.txt
-echo "-------   ------  -------  ---------------------- ---------------------- ---------------------" >> ../output/temp_DnDs_results.txt
+echo "Lineage   Ploidy  Histone  N  S  N/S" > ../output/temp_NS_results.txt
+echo "-------   ------  -------  ---------------------- ---------------------- ---------------------" >> ../output/temp_NS_results.txt
 
 # iterate through each file in the data/deviate_histones_raw directory, setting each filename equal to $f and
 # running the current $f through the analyze_raw_files.py python script
@@ -21,8 +17,6 @@ for f in ../data/deviate_histones_raw/*; do python3 histone_analyzer.py $(basena
 
 # arrange the files in an easier to read format and remove the temp file (temp file has the exact same
 # data as the file results.txt, it's just not as organized/aligned as the new results.txt)
-column -t ../output/temp_PnPs_results.txt > ../output/PnPs_results.txt && rm -f ../output/temp_PnPs_results.txt
+column -t ../output/temp_NS_results.txt > ../output/NS_results.txt && rm -f ../output/temp_NS_results.txt
 
-#do the same for the temp_DnDs_results.txt file!
-column -t ../output/temp_DnDs_results.txt > ../output/DnDs_results.txt && rm -f ../output/temp_DnDs_results.txt
 # DONE :)
